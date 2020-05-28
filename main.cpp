8034e813dd526229651f8572d7700f2ab9bf93fd
Ryan Carins <ryan.carins@riscy.biz>
2020-05-29 01:50:08 +1000

    Split instructions and check if command is NOP as a test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <vector>
+#include <array>
 
 
 std::vector<std::string> load_instruction(std::string filename) {
@@ -16,10 +18,37 @@ std::vector<std::string> load_instruction(std::string filename) {
 	return instructions;
 }
 
-int main(){
-	std::vector<std::string> instructions = load_instruction("instruct.txt");
-	for(int i = 0; i<instructions.size(); i++){
-		std::cout << instructions.at(i) << std::endl;
+//Size is 4 because I'm implementing RiSC-16 instruction set so there will be
+//a max of 4 parts i.e ADD RA, RB, RC https://user.eng.umd.edu/~blj/RiSC/RiSC-isa.pdf
+std::array<std::string,4> getInstruction(std::string line){
+	std::istringstream string_stream(line);
+	std::string curr;
+	std::array<std::string,4> instruction;
+	char delimiter = ' ';
+
+	int i = 0;
+	while(std::getline(string_stream, curr, delimiter) && i<4){
+		instruction[i] = curr;
+		i++;
 	}
+	return instruction;
 }
 
+int main(){
+	//TODO make this an argument
+	std::vector<std::string> instructions = load_instruction("instruct.txt");
+
+	//Memory for CPU I know that RiSC-16 is signed but I don't care
+	int unsigned registers[64];
+
+
+	for(int i = 0; i<instructions.size(); i++){
+		std::array<std::string,4> instruction = getInstruction(instructions.at(i));
+
+		if(instruction[0].compare("NOP") == 0){
+			std::cout << "NOP" << std::endl;
+		}else{
+			std::cout << "Not NOP" << std::endl;
+		}
+	}
+}
